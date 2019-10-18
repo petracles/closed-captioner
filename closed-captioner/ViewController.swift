e28b0d889e6e541a4010520c81f29934e6ce5cd0
petracles <petracles@gmail.com>
2019-10-18 17:26:22 +0200

    wow animated text neato

--- closed-captioner/ViewController.swift
+++ closed-captioner/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let finalPromptLabelText = "Let's help you find the perfect caption"
+    var startIdx = 0
+    
+    let promptLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont(name: "Arial", size: 24)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(promptLabel)
+        promptLabel.frame = view.frame
+        
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink.add(to: .main, forMode: .default)
+        
     }
 
-
+    @objc func handleUpdate() {
+        self.promptLabel.text = String(finalPromptLabelText.prefix(startIdx))
+        startIdx += 1
+    }
 }
 
